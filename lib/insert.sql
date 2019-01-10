6e3576572ac1f8658ab6cae24dcefed0062ff75d
Matthew Sherman <msherman0801@gmail.com>
2019-01-10 21:11:02 +0000

    code

---
+++ lib/insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project 1", "sports", "1", "jan 1", "dec 1");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project 2", "sports", "1", "jan 1", "dec 1");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project 3", "sports", "1", "jan 1", "dec 1");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project 4", "sports", "1", "jan 1", "dec 1");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project 5", "sports", "1", "jan 1", "dec 1");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project 6", "sports", "1", "jan 1", "dec 1");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project 7", "sports", "1", "jan 1", "dec 1");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project 8", "sports", "1", "jan 1", "dec 1");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project 9", "sports", "1", "jan 1", "dec 1");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("project 10", "sports", "1", "jan 1", "dec 1");
